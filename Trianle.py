df7b3dfb7de211052432b1d631ebe8347554afde
Mallinath-dhage <mallinath.dhage.07@gmail.com>
2025-11-19 01:24:16 +0530

    This is the Area of a Triangle program in Python.

---
+++ Trianle.py
@@ -0,0 +1,9 @@
+#To calculate the Area of the Triangle, we need base and hight
+base = float(input("Enter the base:"))
+hight = float(input("Enter the hight:"))
+
+#Now let's calculate area, Area = 1/2 * base * hight
+
+Area = 0.5 * base * hight
+
+print(f"Area of Triangle is:{Area}")
