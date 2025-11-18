79a96e86cb48b71ebf6b46b11873e799daaff8b3
Mallinath-dhage <mallinath.dhage.07@gmail.com>
2025-11-19 01:42:07 +0530

    Added print() explaination

---
+++ Hello.py
@@ -0,0 +1,4 @@
+# Simple Hello World program in Python
+print("Hello, World!")
+
+# Here print function is used to display the output on the screen. It is the most basic function in Python to output text.
