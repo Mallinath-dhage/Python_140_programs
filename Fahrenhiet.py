1c1f1fcb08d60be5a8d9114eae5a21a5374050f9
Mallinath-dhage <mallinath.dhage.07@gmail.com>
2025-11-19 01:48:20 +0530

    3rd program

---
+++ Fahrenhiet.py
@@ -0,0 +1,5 @@
+# Convert the temperature from Celius to Fahrenhiet 
+
+Celius = float(input("Enter temperature in Celius:"))
+Fahrenhiet = (Celius * 9/5) + 32
+print(f"Temperature in Fahrenhiet is:{Fahrenhiet}")
