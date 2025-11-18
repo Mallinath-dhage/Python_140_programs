169831a2f645f64bd3e83ce8425ebfd8e932f3a3
Mallinath-dhage <mallinath.dhage.07@gmail.com>
2025-11-19 01:51:17 +0530

    Added line 6-9

--- Fahrenhiet.py
+++ Fahrenhiet.py
@@ -3,3 +3,7 @@
 Celius = float(input("Enter temperature in Celius:"))
 Fahrenhiet = (Celius * 9/5) + 32
 print(f"Temperature in Fahrenhiet is:{Fahrenhiet}")
+
+# Here we are taking temperature in Celius as input from user and then converting it to Fahrenhiet using the formula (Celius * 9/5) + 32. 
+# Finally, we are printing the result on the screen.
+# here f-string is used to format the output string. f-strings are a way to embed expressions inside string literals, using curly braces {}.
