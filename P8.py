53b585ffdc76101259394bc5e774152c4f00cb4c
Mallinath-dhage <mallinath.dhage.07@gmail.com>
2025-11-24 00:59:38 +0530

    Added Explaination

--- P8.py
+++ P8.py
@@ -2,3 +2,7 @@
 string_input = input("Enter a string: ")
 count_dollar = string_input.count('$')  
 print(f"The occurrence of '$' in the given string is: {count_dollar}")
+
+# Here we are taking a string input from the user and then using the count() method to find the occurrence of the character '$' in that string.
+# Finally, we are printing the result using an f-string to format the output.
+# f-strings are a way to embed expressions inside string literals, using curly braces {}.
