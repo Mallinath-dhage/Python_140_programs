17f607607604d694c8a878221648df7bbe165e5f
Mallinath-dhage <mallinath.dhage.07@gmail.com>
2025-11-24 00:57:33 +0530

    Added Explaination

--- P7.py
+++ P7.py
@@ -2,3 +2,7 @@
 first_name = input("Enter your first name: ")
 length = len(first_name)
 print(f"The length of your first name is: {length}")    
+
+# Here we are taking the user's first name as input and then calculating its length using the len() function.
+# Finally, we are printing the length using an f-string to format the output.
+# f-strings are a way to embed expressions inside string literals, using curly braces {}.
