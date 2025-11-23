ccf52aac04ffeb9145366774242adb59215698ba
Mallinath-dhage <mallinath.dhage.07@gmail.com>
2025-11-24 00:59:00 +0530

    Added program P8

---
+++ P8.py
@@ -0,0 +1,4 @@
+# WAP to find the occurrence of ‘$’ in a String
+string_input = input("Enter a string: ")
+count_dollar = string_input.count('$')  
+print(f"The occurrence of '$' in the given string is: {count_dollar}")
