16f7fe6ea70e9bcbfea913c3b20a00982e3b1345
Mallinath-dhage <mallinath.dhage.07@gmail.com>
2025-11-24 00:55:07 +0530

    Added Program P7

---
+++ P7.py
@@ -0,0 +1,4 @@
+# WAP to input user’s first name & print its length.
+first_name = input("Enter your first name: ")
+length = len(first_name)
+print(f"The length of your first name is: {length}")    
