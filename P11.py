4bdc90a5b1b61136dad52059696fcdd936fc223f
Mallinath-dhage <mallinath.dhage.07@gmail.com>
2025-11-28 22:26:22 +0530

    Added P11.py

---
+++ P11.py
@@ -0,0 +1,7 @@
+# WAP to check if a number is a multiple of 7 or not.
+
+num = int(input("Enter any random number:"))
+if(num % 7 == 0):
+    print(f"Given number",{num},"is multiple of 7.")
+else:
+    print("Try again!!, With different number.")
