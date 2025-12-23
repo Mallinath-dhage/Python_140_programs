5cea98f4271f93cbd73558b1f3fee586a37e3d7d
Mallinath-dhage <mallinath.dhage.07@gmail.com>
2025-12-23 23:48:14 +0530

    updated Armstrong.py and added the explaination

--- Armstrong.py
+++ Armstrong.py
@@ -7,12 +7,20 @@ temp = num
 if temp <= 0:
     print("Only positive numbers allowed")
 
+sum = 0
 while temp > 0:
     d = temp % 10
     sum += d**nd
     temp //= 10
 
 if sum == num:
-    print(f"Given number", {num} "is an Armstrong number.")
+    print(f"Given number", {num}, "is an Armstrong number.")
 else:
-    print(f"Given number"  {num} "is not an Armstrong number.")
+    print(f"Given number", {num}, "is not an Armstrong number.")
+
+#In line 5, converted the number to string to find the length of the number which is used as power while calculating the sum of digits raised to the power of number of digits.
+#In line 12, calculated the number of digits only for positive numbers. using the modular operator to extract each digit and raising it to the power of number of digits and adding it to sum.
+#In line 13, Calculated the sum of each digit raised to the power of number of digits.
+#In line 14, removed the last digit from the number by performing integer floor division by 10.
+#In line 16-20, compared the calculated sum with the original number to determine if it is an Armstrong number or not and printed the result.
+#The program handles only positive integers as input.
